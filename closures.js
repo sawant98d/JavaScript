eb913a26e4f8fe154a935d9a788ecd68b70cbad0
sawant98d <sawant98d@gmail.com>
2020-12-26 21:55:50 +0530

    done with execution context

---
+++ closures.js
@@ -0,0 +1,12 @@
+
+
+var n  = 2;
+function square(num){
+    var ans = num*num;
+    return ans;
+}
+
+var square2 = square(n);
+var square4 = square(4);
+console.log('Square of 2 is '+square2);
+console.log('Square of 4 is '+square4);
