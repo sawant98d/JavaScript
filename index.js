472be3c54fb71cc2712155b8bca9e5d1ba54f0e7
sawant98d <sawant98d@gmail.com>
2020-12-27 12:27:54 +0530

    Demo of Call Stack in the Browser

--- index.js
+++ index.js
@@ -1,25 +1,11 @@
 
 
-
-console.log(x);
-//console.log(getName);
-
 var x = 7;
 
-
-
-
-var getName1 = () => {
-    console.log("Namaste JavaScript 1");
+function getName(){
+    console.log("Namaste Javascript");
 }
 
-function getName2(){
-    console.log("Namaste JavaScript 2");
-}
-
-var getName3 = function() {
-    console.log("Namaste JavaScript 3");
-}
-
-
-getName1();
+getName();
+console.log(x);
+console.log(getName);
