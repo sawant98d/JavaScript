f6591448c3ba9db73021db3bff8d3b403989004d
sawant98d <sawant98d@gmail.com>
2020-12-27 22:27:34 +0530

    The scope chain, scope and lexical environment in js

--- index.js
+++ index.js
@@ -1,33 +1,11 @@
 
-// this is shorted js program 
-// console.log(window==this); // ans is 'true'
 
-
-/*
-var a = 10;
-
-function b(){
-    var x  = 100;
-    console.log(x);
+function a(){
+    var b = 10;
+    c();
+    function c(){
+        console.log(b);
+    }
 }
 
-console.log(a);
-console.log(window.a);
-console.log(this.a);
-//console.log(x);//error here because x is not in Global Space Memory
-
-*/
-
-var a ;
-console.log(a);
-a=10;
-
-if(a === undefined){
-    console.log("a is undefined");
-}
-else{
-    console.log("a is not undefined");
-}
-
-console.log(a);
-//console.log(x); // gives an error because it is not defined in execution context
+a();
