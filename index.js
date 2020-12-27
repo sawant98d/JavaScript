a5324b46a28f15d5a43937816b072b05e16459f7
sawant98d <sawant98d@gmail.com>
2020-12-27 16:59:41 +0530

    Shortest JS Program window and this keyword

--- index.js
+++ index.js
@@ -1,16 +1,2 @@
 
-var x  = 1;
-
-a();
-b();
-console.log(x);
-
-function a(){
-    var x = 10;
-    console.log(x);
-}
-
-function b(){
-    var x = 100;
-    console.log(x);
-}
+// this is shorted js program 
