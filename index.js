61bb561a2711efa297c77299506a5d3739d716a6
sawant98d <sawant98d@gmail.com>
2021-01-02 18:10:26 +0530

    block scope and showdowing scope in JS

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 
-let a = 10;
+var a = 100;  // creates in Global scope
+let b = 200; // creates in Script scope
+{
+    // Compound Statement
+    var a = 10;   // showdowing of the var  a :: Refers to the global scope var a
+    let b = 20;   // here b and c are created in Block Scope
+    const c = 30;
+    console.log(a);
+    console.log(b);
+    console.log(c);
+}
 console.log(a);
-var b = 100;
-// only var can be created in Global Memory Space
-// let and const are created in script;
-//
-
-// Temporal Dead Zone : the time between the let var hoiseted and initilized it some value
+console.log(b);
+console.log(c);
