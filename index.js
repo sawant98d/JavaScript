68386d0c028b7f3c02cb1846417a4d9538ca75f9
sawant98d <sawant98d@gmail.com>
2021-01-03 13:43:46 +0530

    closures in js;

--- index.js
+++ index.js
@@ -1,31 +1,27 @@
-
-var a = 100;  // creates in Global scope
-let b = 200; // creates in Script scope
-const c = 300;
-{
-    // Compound Statement
-    var a = 10;   // shawdowing of the var  a :: Refers to the global scope var a
-    let b = 20;   // here b and c are created in Block Scope
-    const c = 30;
-    console.log(a);
-    console.log(b);
-    console.log(c);
-}
-
-console.log(c);
-
-
-var k = 10;
-{
-    let k = 40; // Illegel shadowing
-}
-
-
-const z  = 20;
-{
-    const z = 100;
-    {
-        const z = 200;
-        console.log(a); // It follows the lexical code chain pattern:)
+function x(){
+    var a  = 7;
+    function y(){
+        console.log(a);
     }
+    return y;
 }
+var z = x();
+console.log(z);
+z();
+
+
+function z(){
+    var a = 10;
+    function y(){
+        var b = 20;
+        function x(){
+            var c = 30;
+            console.log(a);
+            console.log(b);
+        }
+        x();
+    }
+    y();
+}
+
+z();
